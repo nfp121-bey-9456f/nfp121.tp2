bffdf2241c8db22427f0b52decfb3adef5426bb1
Elie DAHER <elie.daher@isae.edu.lb>
2019-03-19 17:53:24 +0200

    achèvement de la 2me question

--- tp2/question2/FahrenheitCelsius.java
+++ tp2/question2/FahrenheitCelsius.java
@@ -1,38 +1,40 @@
 package question2;
 
-
 /**
  * Décrivez votre classe FahrenheitCelsius ici.
  * 
- * @author (votre nom) 
- * @version (un numéro de version ou une date)
+ * @author Elie DAHER 
+ * @version v1.0
  */
-public class FahrenheitCelsius{
 
-     /** le point d'entrée de cette application, 
-      * dont le commentaire est à compléter
-      *
-      *  @param args ...
-      */
-     public static void main(String[] args){
-       try{
-       
-      
-       }catch(NumberFormatException nfe){
-           System.out.println("error : " + nfe.getMessage());  // en cas d'erreur 
-       }
-       
-     }
-     
-     /** 
-      * la méthode à compléter. 
-      *   @param f la valeur en degré Fahrenheit
-      *   @return  la conversion en degré Celsius
-      */
-     public static float fahrenheitEnCelsius( int f){
-       // ...
-       return 0.F;	// à compléter	en remplaçant la valeur retournée par la fonction de conversion
-       // ...
-     }
+public class FahrenheitCelsius {
+    /**
+     * Affiche la conversion en Celsius d'une liste de valeurs en Fahrenheit
+     * @param args liste des températures en Fahrenheit a convertir en Celsius 
+     * et affiche un message en cas d'erreur
+     */
+    public static void main(String[] args) {
+        try {
+            for (int i=0;i<args.length;i++){
+                int fahrenheit = Integer.parseInt(args[i]);
+                float celsius = fahrenheitEnCelsius(fahrenheit);
+                System.out.println(fahrenheit + "\u00B0F -> " + celsius + "\u00B0C");
+            }
+        }
+        catch(NumberFormatException nfe){
+            System.out.println("Erreur Entrée non convertible: " + nfe.getMessage());  // en cas d'erreur 
+        }
 
+    }
+
+    /**
+     * Méthode pour convertir Fahrenhiet en Celsius.
+     * @param f la valeur en degré Fahrenheit (°F)
+     * @return la conversion en degré Celsius (°C)
+     */
+    public static float fahrenheitEnCelsius(int f) {
+        float value = (f - 32) * 5F/9F; // conversion implicite en float pour éviter une division entière et perdre les décimales
+        return (int)(value * 10) / 10F; // troncation de la valeur à une decimale
+
+    }
 }
